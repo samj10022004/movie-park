e8f9dfbc5106f4b338dd32cfbaa814d77b857330
samj10022004 <97349711+samj10022004@users.noreply.github.com>
2024-05-30 02:11:12 +0530

    towards final

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,25 +1,35 @@
 import { useState } from "react";
 import useFetch from "../../hooks/useFetch";
-import "./style.scss"
+import "./style.scss";
 import { useParams } from "react-router-dom";
-import DetailsBanner from "./detailsBanner/DetailsBanner"
-import Cast from "./cast/Cast"
+import DetailsBanner from "./detailsBanner/DetailsBanner";
+import Cast from "./cast/Cast";
+import VideoSection from "./videoSection/VideoSection";
+import Similar from "./carousels/Similar";
+import Recommendation from "./carousels/Recommendation";
 
 const Details = () => {
+    const { mediaType, id } = useParams();
+    const { data: videosData, loading: videosLoading } = useFetch(`/${mediaType}/${id}/videos`);
+    const { data: creditsData, loading: creditsLoading } = useFetch(`/${mediaType}/${id}/credits`);
 
-  const {mediaType,id}=useParams();
-  const {data,loading}=useFetch(`/${mediaType}/${id}/videos`);
-  const {data:credits,loading:creditsLoading}=useFetch(`/${mediaType}/${id}/credits`);
-  
+    return (
+        <div>
+            <DetailsBanner video={videosData?.results?.[0]} crew={creditsData?.crew} />
+            {creditsData?.cast?.length > 0 && (
+                <Cast data={creditsData.cast} loading={creditsLoading} />
+            )}
+            {videosData?.results?.length > 0 && (
+                <VideoSection data={videosData} loading={videosLoading} />
+            )}
+            {mediaType && id && (
+                <>
+                    <Similar mediaType={mediaType} id={id} />
+                </>
+            )}
+            <Recommendation mediaType={mediaType} id={id} />
+        </div>
+    );
+};
 
-
-  return (
-    <div>
-      <DetailsBanner video={data?.results?.[0]}  crew={credits?.crew} />
-      <Cast data={credits?.cast} loading={creditsLoading}/>
-      
-    </div>
-  )
-}
-
-export default Details
+export default Details;
